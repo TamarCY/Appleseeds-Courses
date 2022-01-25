229cc36b4edc711c65ccc87e4e152c367c2fe398
tamarcy <cohen.tamar@gmail.com>
2022-01-25 15:24:02 +0200

    connecting to chack atlas react and heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+// require("dotenv").config({ path: path.resolve(_dirname,"../.env")})
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const path = require("path");
+// TODO: add require to the userRouter file
+
 const app = express();
 
 const publicPath = path.join(__dirname, "../client/build");
@@ -11,10 +15,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(publicPath));
 
+//
+const mongo_uri = process.env.MONGO_URI;
+
+//
+mongoose
+  .connect(mongo_uri)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
+
+// TODO: add app.use("/", userRouter)
+
 app.use("*", (req, res) => {
-	res.send("Wrong route");
+  res.send("Wrong route");
 });
+
+
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+
